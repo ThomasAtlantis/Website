86d16d21fb3529921dd69de0fdbe1449d4d9890c
清川 <qingchuan@MacBookPro.local>
2025-09-12 20:49:13 +0800

    fix: vite.config.ts github pages doesn't support vite router

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import fs from 'fs'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // 自定义插件：在构建后生成 _redirects 文件
-    {
-      name: 'generate-redirects',
-      writeBundle() {
-        const redirectsContent = '/*    /index.html   200'
-        fs.writeFileSync('dist/_redirects', redirectsContent)
-      }
-    }
-  ],
+  base: '/Website/',
+  plugins: [react()],
   css: {
     preprocessorOptions: {
       less: {
